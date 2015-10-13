940d9fd27fd42e3d28dfd9ed814c021b68d31066
kcc15a <kcc15a@acu.edu>
2015-10-13 13:10:07 -0500

    New Game Scene File
    I made a new file for the game scene and im basing it off of the default
    game scene.

---
+++ game.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------------------
+--
+-- level1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+-- include Corona's "physics" library
+local physics = require "physics"
+physics.start(); physics.pause()
+
+--------------------------------------------
+
+-- forward declarations and other locals
+local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+
+function scene:create( event )
